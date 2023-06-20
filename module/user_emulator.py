97df939987a53c9d427d69099f7a11bd4204d29c
18933996 <oddryabkov@sberbank.ru>
2023-06-20 15:22:50 +0300

    popup worker, randomizer for time.sleep for more user exp

--- module/user_emulator.py
+++ module/user_emulator.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import config
+import random
 import time
 
 
@@ -11,9 +12,27 @@ class ResearchParser:
         cred = config.research_cred
         research_base_url = config.research_base_url
         tabs = {'all': 'all', 'everyday': '905--910--900', 'reviews': '3--0--5--106--15--63--81--87'}
-        self.cred = cred
+        base_popup_xpath = '/html/body/div[2]/div/div/div/div/div/div/div[3]'
+        self.base_popup_xpath = base_popup_xpath
         self.research_base_url = research_base_url
         self.tabs = tabs
+        self.cred = cred
+
+    @staticmethod
+    def __sleep_some_time(start: int = 1, end: int = 2):
+        time.sleep(random.uniform(start, end))
+
+    def popup_worker(self, table, driver):
+        table.click()
+        self.__sleep_some_time(2, 3)
+        review_page = driver.find_element(By.XPATH, self.base_popup_xpath)
+        rows = review_page.find_elements('tag name', 'p')
+        review_first = driver.find_element(By.XPATH, '{}/p[1]'.format(self.base_popup_xpath)).text
+        review_last = driver.find_element(By.XPATH, '{}/p[{}]'.format(self.base_popup_xpath, len(rows))).text
+        if len(review_last) < 5:
+            review_last = driver.find_element(By.XPATH, '{}/p[{}]'.format(self.base_popup_xpath, len(rows)-1)).text
+        review_page.send_keys(Keys.ESCAPE)
+        return [table.text, '{} {}'.format(review_first, review_last)]
 
     def auth(self, driver):
         login = self.cred[0]
@@ -38,31 +57,30 @@ class ResearchParser:
         wait = WebDriverWait(driver, 10)
         assert 'Экономика' in driver.title
         wait.until(ec.presence_of_element_located((By.ID, self.tabs['everyday'])))  # Ежедневные
-        time.sleep(1)
+        self.__sleep_some_time()
+
         driver.find_element('id', '905--910--900').click()
         wait.until(ec.presence_of_element_located((By.CLASS_NAME, 'title')))
-        time.sleep(1)
+        self.__sleep_some_time()
+
         table = driver.find_elements('class name', 'title')
         for i in table[1:6:2]:
-            time.sleep(2)
-            print(i.text)
-            i.click()
-            time.sleep(2)
-            review = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/div/div/div[3]')
-            review_1 = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/div/div/div[3]/p[1]').text
-            review_3 = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/div/div/div[3]/p[3]').text
-            review.send_keys(Keys.ESCAPE)
-            reviews.append('{}\n{}'.format(review_1, review_3))
+            self.__sleep_some_time(2, 3)
+            # print(i.text)
+            reviews.append(self.popup_worker(i, driver))
         return reviews
 
     def get_eco_review(self, driver, url):
         review = []
         driver.get(url)
         wait = WebDriverWait(driver, 10)
-        time.sleep(2)
+        self.__sleep_some_time(2, 3)
         wait.until(ec.presence_of_element_located((By.ID, self.tabs['reviews'])))  # Обзоры
         driver.find_element('id', self.tabs['reviews']).click()
-        time.sleep(2)
+        self.__sleep_some_time(2, 3)
+        table = driver.find_element(By.XPATH, '//*[@id="publicationsTable"]/tbody/tr[2]/td[1]/div/a')
+        review.append(self.popup_worker(table, driver))
+        '''
         eco_xpath = '//*[@id="publicationsTableContainer"]/div[2]'
         last_eco_review = driver.find_element(By.XPATH, eco_xpath)
         last_eco_review.click()
@@ -71,4 +89,5 @@ class ResearchParser:
         # last_eco_review.send_keys(Keys.ESCAPE)
         review.append([last_eco_review.text, eco_review_text])
         print(review)
+        '''
         return review
