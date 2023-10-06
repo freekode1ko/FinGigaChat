612476fdbe50260a00e9ccb34272bf96479c9706
18933996 <oddryabkov@sberbank.ru>
2023-10-06 15:59:08 +0300

    PEP8

--- module/data_transformer.py
+++ module/data_transformer.py
@@ -9,6 +9,7 @@ import copy
 import matplotlib.ticker as ticker
 from bot_runner import read_curdatetime
 
+
 class Transformer:
 
     @staticmethod
@@ -82,16 +83,17 @@ class Transformer:
             size = None
 
             if ax is None:
-                row_height=1
+                row_height = 1
                 size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
                 y_delta = size[1] + 0.145
-                size = (15,y_delta)
+                size = (15, y_delta)
                 fig, ax = plt.subplots(figsize=size)
                 fig.patch.set_facecolor('black')
                 ax.axis('off')
 
-            mpl_table = ax.table(cellText=data.values, bbox=[-0.17, -0.2, 1.3, 1.145], colLabels=data.columns, colWidths=[0.2, 0.05, 0.05, 0.05, 0.05],
-                                cellLoc='center', **kwargs)
+            mpl_table = ax.table(cellText=data.values, bbox=[-0.17, -0.2, 1.3, 1.145], colLabels=data.columns,
+                                 colWidths=[0.2, 0.05, 0.05, 0.05, 0.05],
+                                 cellLoc='center', **kwargs)
             plt.subplots_adjust(bottom=0.25)
             mpl_table.auto_set_font_size(False)
             mpl_table.set_fontsize(font_size)
@@ -106,17 +108,19 @@ class Transformer:
                     cell.set_text_props(fontsize=18)
                     cell.set_facecolor(row_colors[k[0] % len(row_colors)])
                     cell.get_text().set_color('white')
-            
+
             title_loc = 'center'
             ax.text(0.5, 1.1, alias, fontsize=20, fontweight='bold',
-                    color='white', ha=title_loc, va='top', transform=ax.transAxes, bbox=dict(facecolor='none', edgecolor='none'), 
+                    color='white', ha=title_loc, va='top', transform=ax.transAxes,
+                    bbox=dict(facecolor='none', edgecolor='none'),
                     clip_on=False)
-            
+
             sample_of_img_title_view = 'Sber Analytical Research. Данные на {}*'
             sample_of_img_title_view = sample_of_img_title_view.format(read_curdatetime().split()[0])
             title_loc = 'left'
             ax.text(-0.1, -0.245, sample_of_img_title_view, fontsize=10, fontweight='bold',
-                    color='white', ha=title_loc, va='top', transform=ax.transAxes, bbox=dict(facecolor='none', edgecolor='none'), 
+                    color='white', ha=title_loc, va='top', transform=ax.transAxes,
+                    bbox=dict(facecolor='none', edgecolor='none'),
                     clip_on=False)
         else:
             if ax is None:
@@ -129,7 +133,7 @@ class Transformer:
                 ax.axis('off')
 
             mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns,
-                                cellLoc='center', **kwargs)
+                                 cellLoc='center', **kwargs)
             mpl_table.auto_set_font_size(False)
             mpl_table.set_fontsize(font_size)
 
@@ -150,16 +154,16 @@ class Transformer:
     def __draw_plot(df, name):
         labels = []
         xticks = []
-        for i,date in enumerate(df['date']):
+        for i, date in enumerate(df['date']):
             date_obj_year = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S').year
             if date_obj_year not in labels:
                 labels.append(date_obj_year)
                 xticks.append(df.iloc[i]['x'])
 
-        while len(labels)>5:
+        while len(labels) > 5:
             del labels[0]
             del xticks[0]
-    
+
         first = df.loc[df['x'] == xticks[0], 'x'].index[0]
         fig, ax = plt.subplots()
         fig.canvas.draw()
@@ -169,7 +173,7 @@ class Transformer:
 
         labels_new = []
         for i, label in enumerate(labels):
-            if i!= len(labels)-1:
+            if i != len(labels) - 1:
                 labels_new.append(label)
                 labels_new.append('')
             else:
@@ -178,16 +182,17 @@ class Transformer:
 
         xticks_new = []
         for i, xtick in enumerate(xticks):
-            if i!= len(labels)-1:
+            if i != len(labels) - 1:
                 xticks_new.append(xtick)
-                xticks_new.append((xticks[i+1]-xticks[i])/2 + xtick)
+                xticks_new.append((xticks[i + 1] - xticks[i]) / 2 + xtick)
             else:
                 xticks_new.append(xtick)
-                xticks_new.append((xticks[i]-xticks[i-1])/2 + xtick)
+                xticks_new.append((xticks[i] - xticks[i - 1]) / 2 + xtick)
 
         minor_locator = ticker.AutoMinorLocator(n=2)
         plt.gca().yaxis.set_minor_locator(minor_locator)
         plt.gca().tick_params(which='minor', length=4, color='black', width=1)
+
         def format_yticks(value, pos):
             return '{:,.0f}'.format(value).replace(',', ' ')
 
@@ -198,38 +203,38 @@ class Transformer:
         ax.set_xticklabels(labels_new)
         ax.yaxis.set_tick_params(length=0)
 
-        color = (30/255, 212/255, 132/255)
+        color = (30 / 255, 212 / 255, 132 / 255)
         ax.plot(df['x'][first:], df['y'][first:], color=color)
         ax.yaxis.tick_right()
 
-        plt.xlim(df['x'].iloc[first],df['x'].iloc[-1])
+        plt.xlim(df['x'].iloc[first], df['x'].iloc[-1])
 
         y = df['y'].iloc[-1]
         ax.axhline(y=y, color=color, linestyle='dotted')
 
         x = df['x'].iloc[-1]
-        delta_x = (x/100)
+        delta_x = (x / 100)
         y = round(y, 1)
-        delta_y = (y/100)*5
+        delta_y = (y / 100) * 5
         x_name = df['x'].iloc[first]
         y_name = df['y'][first:].max()
 
         ax.text(x_name, y_name, name, fontsize=12)
-        ax.text(x-delta_x, y+delta_y, y,fontsize=10, weight='bold')
+        ax.text(x - delta_x, y + delta_y, y, fontsize=10, weight='bold')
         ax.plot(x, y, 'o', markersize=6, color=color)
 
     @staticmethod
     def five_year_graph(data, name):
-        if isinstance(data,pd.DataFrame):
+        if isinstance(data, pd.DataFrame):
             Transformer.__draw_plot(data, name)
         else:
             df = pd.DataFrame(data.json()['series'][0]['data'])
             Transformer.__draw_plot(df, name)
 
-        name = name.replace('/','_')
-        name = name.replace(' ','_')
-        name = name.split(',')  
-        name =f'{name[0]}_graph.png'
+        name = name.replace('/', '_')
+        name = name.replace(' ', '_')
+        name = name.split(',')
+        name = f'{name[0]}_graph.png'
         # save png and return it to user
         png_path = '{}/img/{}'.format('./sources', name)
         plt.savefig(png_path, transparent=False)
@@ -239,12 +244,12 @@ class Transformer:
         """
         Transform unix-time to world-time
         """
-        
+
         date_time = datetime.datetime.fromtimestamp(timestamp / 1000)
         formatted_date = date_time.strftime('%Y-%m-%dT%H:%M:%S')
 
         return formatted_date
-    
+
     @staticmethod
     def default_to_unix():
         """
@@ -254,15 +259,15 @@ class Transformer:
         now = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         date_time = datetime.datetime.strptime(now, '%Y-%m-%d %H:%M:%S')
         unix_timestamp = int(date_time.timestamp())
-        
+
         return str(unix_timestamp)
-    
+
     @staticmethod
     def url_updater():
         """
         Create urls to charts 
         """
-        
+
         unix_timestamp = Transformer.default_to_unix()
         charts_links = copy.deepcopy(config.charts_links)
         commodities = copy.deepcopy(config.dict_of_commodities)
@@ -274,6 +279,7 @@ class Transformer:
             elif commodities[commodity]['naming'] != 'Gas':
                 name = commodities[commodity]['links'][0]
                 commodities[commodity]['links'][0] = charts_links['metals_wire_link'].replace('name_name', name)
-                commodities[commodity]['links'][0] = commodities[commodity]['links'][0].replace('date_date', unix_timestamp)
+                commodities[commodity]['links'][0] = commodities[commodity]['links'][0].replace('date_date',
+                                                                                                unix_timestamp)
 
         return commodities
