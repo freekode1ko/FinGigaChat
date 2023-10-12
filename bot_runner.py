2377f0a4b48d5fd9fd6de0988fb495e8bc6d358a
18933996 <oddryabkov@sberbank.ru>
2023-10-12 11:53:43 +0300

    !!!DO NOT MERGE THIS BRANCH!!! upd imports

--- bot_runner.py
+++ bot_runner.py
@@ -1,6 +1,6 @@
-import warnings
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
+import warnings
 import config
 
 API_TOKEN = config.api_token
@@ -14,7 +14,10 @@ dp = Dispatcher(bot, storage=storage)
 @dp.message_handler()
 async def giga_ask(message: types.Message):
     print('{} - {}'.format(message.from_user.full_name, message.text))
-    await message.answer('Бот не доступен, обратитесь позже', protect_content=True)
+    await message.answer('Добрый день! В настоящий момент на '
+                         'сервере ведутся технические работы и загружаются обновления. '
+                         'Приносим свои извинения за доставленные неудобства! \n\n'
+                         'Прямо сейчас мы делаем наш бот лучше!', protect_content=True)
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
