f5fb2a8861c9038da5cb4a6f39b957b6c9a16952
18933996 <oddryabkov@sberbank.ru>
2023-10-13 15:40:55 +0300

    data fix for USDOLLAR and usd-rub

--- main.py
+++ main.py
@@ -244,9 +244,13 @@ class Main:
     def exchange_block(self, table_exchange: list, exchange_page: str, session: req.sessions.Session):
         exchange_kot = []
         if table_exchange[0] == 'Курсы валют' and exchange_page in ['usd-rub', 'eur-rub', 'cny-rub', 'eur-usd']:
-            if {'Exchange', 'Last', 'Time'}.issubset(table_exchange[3].columns):
-                row = [exchange_page, table_exchange[3].loc[table_exchange[3]['Exchange'] ==
-                                                            'Real-time Currencies']['Last'].values.tolist()[0]]
+            if exchange_page == 'usd-rub':
+                table_num = 5
+            else:
+                table_num = 3
+            if {'Exchange', 'Last', 'Time'}.issubset(table_exchange[table_num].columns):
+                row = [exchange_page, table_exchange[table_num].loc[table_exchange[table_num]['Exchange'] ==
+                                                                    'Real-time Currencies']['Last'].values.tolist()[0]]
                 exchange_kot.append(row)
 
         elif table_exchange[0] == 'Курсы валют' and exchange_page in ['usd-cny', 'usdollar']:
@@ -254,7 +258,8 @@ class Main:
             tree = html.fromstring(page_html)
 
             # USDOLLAR
-            object_xpath = '//*[@id="__next"]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div[1]'
+            object_xpath = '//*[@id="__next"]/div[2]/div[3]/div[1]/div[1]/div[3]/div/div[1]/div[1]/div[1]'
+            # '//*[@id="__next"]/div[2]/div[2]/div[1]/div[1]/div[3]/div/div[1]/div[1]' -old
             price = tree.xpath('{}/text()'.format(object_xpath))
             # usd-cny
             if not price:
