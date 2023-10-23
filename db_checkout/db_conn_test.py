1742029b17a63481826d18bf3ae10d2b372ce8fd
18933996 <oddryabkov@sberbank.ru>
2023-10-23 15:06:12 +0300

    new creation

--- db_checkout/db_conn_test.py
+++ db_checkout/db_conn_test.py
@@ -4,10 +4,13 @@ import pandas as pd
 psql_engine = 'postgresql://testuser:12345678A@77.232.134.41:5432/users'
 engine = create_engine(psql_engine)
 
-query = ('CREATE TABLE IF NOT EXISTS public.test '
-         '(id integer NOT NULL GENERATED ALWAYS AS IDENTITY '
-         '( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ), '
-         'name text NOT NULL, CONSTRAINT test_pkey PRIMARY KEY (id)) '
+query = ('CREATE TABLE IF NOT EXISTS public.whitelist ('
+         'user_id integer NOT NULL, '
+         'username text NOT NULL, '
+         'email text, '
+         'user_type text NOT NULL, '
+         'user_status text NOT NULL, '
+         'CONSTRAINT user_id_pkey PRIMARY KEY (user_id)) '
          'TABLESPACE pg_default;')
 with engine.connect() as conn:
     conn.execute(text(query))
