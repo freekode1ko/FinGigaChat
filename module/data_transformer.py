b536639de295bae91f580615411fe7bd30767c73
18933996 <oddryabkov@sberbank.ru>
2023-12-08 17:21:15 +0300

    Убрано лишнее Сортировка либ оптимизирован метод formatter

--- module/data_transformer.py
+++ module/data_transformer.py
@@ -1,15 +1,12 @@
+from bot_runner import read_curdatetime
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import datetime
-import copy
-
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import pandas as pd
-from pandas.plotting import table
-import six
-
-from bot_runner import read_curdatetime
 import config
+import copy
+import six
 
 
 class Transformer:
@@ -38,58 +35,24 @@ class Transformer:
         return pd.read_html(html_rep, decimal=',', thousands='.')
 
     @staticmethod
-    def save_df_as_png(df: pd.DataFrame, path_to_source: str,
-                       figure_size: tuple, column_width: list,
-                       fontsize=10, tabla_scale=(1.2, 1.2), name='') -> None:
-        """
-        Get png of DataFrame as table
-        :param df: DataFrame for converting in png
-        :param path_to_source: Path to where save png
-        :param figure_size: Size of png space. Tuple(Width, Height)
-        :param column_width: Width of all columns. [column1_width, column2_width, ...]
-        :param fontsize: Fontsize for text in table
-        :param tabla_scale: Scale column widths by xscale and row heights by yscale. (x_scale, y_scale)
-        :param name: Name of saved file. By default, will save as /_table.png
-        :return: None
-        """
-        # generate table as png
-        fig, ax = plt.subplots(figsize=figure_size)
-        ax.xaxis.set_visible(False)
-        ax.yaxis.set_visible(False)
-        ax.set_frame_on(False)
-        tabla = table(ax, df, loc='upper right', colWidths=column_width)
-        tabla.auto_set_font_size(False)
-        tabla.set_fontsize(fontsize)
-        tabla.scale(tabla_scale[0], tabla_scale[1])
-
-        # save png and return it to user
-        png_path = '{}/img/{}_table.png'.format(path_to_source, name)
-        plt.savefig(png_path, transparent=True)
-
-    @staticmethod
     def formatter(f):
         """
         Format value
         :param f: value to format
         :return: formatted value
         """
-
         try:
             f = float(f)
-        except:
-            pass
+        except ValueError:
+            return str(f)
 
         if pd.isna(f):
             return 'NaN'
-
         elif isinstance(f, (int, float)):
             if abs(f) < 1000:
                 return '{:,.1f}'.format(f).replace('.00', '').replace('.0', '').replace(',', ' ')
             else:
                 return '{:,.0f}'.format(f).replace('.00', '').replace('.0', '').replace(',', ' ')
-
-        else:
-            return str(f)
 
     @staticmethod
     def render_mpl_table(data, name, col_width=1.0, row_height=0.625, font_size=14,
@@ -99,13 +62,6 @@ class Transformer:
         data = data.fillna('-')
         if title is None:
             title = name
-
-        # titles = [title]*len(data.columns.tolist())
-        # orig_columns = data.columns.tolist()
-        # columns = [(titles[i], orig_columns[i]) for i in range(0, len(titles))]
-        # data = pd.DataFrame(data=data.values.tolist(),
-        #                    columns=pd.MultiIndex.from_tuples(columns, names=['title', 'columns']))
-
         if alias:
             size = None
             bbox = [-0.17, -0.2, 1.3, 1.145]
@@ -183,9 +139,6 @@ class Transformer:
                 size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
                 fig, ax = plt.subplots(figsize=size)
                 fig.facecolor = 'black'
-                # rect = patches.Rectangle((-100, -100), 70, 70, linewidth=1, edgecolor='r', facecolor='black', )
-                # ax.add_patch(rect)
-                # ax.set_title(title, color='black')
                 ax.axis('off')
 
             mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns,
