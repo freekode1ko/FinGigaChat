c633aac0d638c75544122277b7b50ce67192d8fb
Gerfurmur <timur71398@gmail.com>
2024-02-06 11:56:06 +0300

    Несколько попыток при отправке post запроса
    Signed-off-by: Gerfurmur <timur71398@gmail.com>

--- main_article_online.py
+++ main_article_online.py
@@ -15,12 +15,43 @@ from utils import sentry
 PERIOD = 1
 
 
+def try_post_n_times(n: int, **kwargs) -> requests.Response:
+    """Отправляет post запрос при помощи requests, совершает n-1 попыток с перехватом ошибки"""
+    for _ in range(n - 1):
+        try:
+            return requests.post(**kwargs)
+        except requests.ConnectTimeout as e:
+            error_msg = f'Время ожидания запроса истекло при попытке подключения к удаленному серверу: {e}'
+            logger.error(error_msg)
+            print(error_msg)
+        except requests.Timeout as e:
+            error_msg = f'Время ожидания запроса истекло: {e}'
+            logger.error(error_msg)
+            print(error_msg)
+        except requests.TooManyRedirects as e:
+            error_msg = f'Слишком много перенаправлений: {e}'
+            logger.error(error_msg)
+            print(error_msg)
+        except requests.ConnectionError as e:
+            error_msg = f'Возникла ошибка соединения: {e}'
+            logger.error(error_msg)
+            print(error_msg)
+        except requests.RequestException as e:
+            error_msg = f'При обработке запроса произошло неоднозначное исключение: {e}'
+            logger.error(error_msg)
+            print(error_msg)
+
+        time.sleep(config.POST_TO_GIGAPARSER_SLEEP_AFTER_ERROR)
+
+    return requests.post(**kwargs)
+
+
 def get_article() -> pd.DataFrame:
     """Получение новостей"""
     df_article = pd.DataFrame()
     try:
         url = BASE_GIGAPARSER_URL.format('get_articles/all')
-        req = requests.post(url, timeout=config.POST_TO_GIGAPARSER_TIMEOUT)
+        req = try_post_n_times(config.POST_TO_GIGAPARSER_ATTEMPTS, url=url, timeout=config.POST_TO_GIGAPARSER_TIMEOUT)
         if req.status_code == 200:
             df_article = df_article.from_dict(req.json())
         else:
@@ -77,7 +108,12 @@ def post_ids(ids):
     try:
         logger.debug('Отправка id обработанных новостей на сервер')
         # ids = {'id': [1,2,3...]}
-        requests.post(BASE_GIGAPARSER_URL.format('success_request'), json=ids, timeout=config.POST_TO_GIGAPARSER_TIMEOUT)
+        try_post_n_times(
+            config.POST_TO_GIGAPARSER_ATTEMPTS,
+            url=BASE_GIGAPARSER_URL.format('success_request'),
+            json=ids,
+            timeout=config.POST_TO_GIGAPARSER_TIMEOUT
+        )
     except Exception as e:
         print(f'Ошибка при отправке id обработанных новостей на сервер: {e}')
         logger.error('Ошибка при отправке id обработанных новостей на сервер: %s', e)
