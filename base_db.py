badccadc0532b41396df18fc87978e5bd4ca60d2
Alex Gavrilov <alex@Alexs-MacBook-Pro.local>
2024-03-11 14:47:36 +0300

    add column to database script

--- base_db.py
+++ base_db.py
@@ -50,7 +50,8 @@ def main(engine):
         'id integer NOT NULL GENERATED ALWAYS AS IDENTITY '
         '( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),'
         'name text NOT NULL,'
-        'CONSTRAINT client_pkey PRIMARY KEY (id)'
+        'CONSTRAINT client_pkey PRIMARY KEY (id),'
+        'navi_link text,'
         ')'
         'TABLESPACE pg_default;'
     )
