5a2235747003c5f27f83554f9feff30a8ea3001e
18933996 <oddryabkov@sberbank.ru>
2024-04-04 17:06:12 +0300

    Обработчик таблиц

--- src/research/module/data_transformer.py
+++ src/research/module/data_transformer.py
@@ -348,6 +348,32 @@ class Transformer:
                 return lst[i - 1:]
         return lst
 
+    def process_fin_summary_table(self, page_html: str, company_id: int, metadata_df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Обработка страницы сектора. Попытка вытащить из нее таблицы P&L (Прибыль и убытки), Баланс и Денежный поток
+
+        :param page_html:
+        :param company_id:
+        :param metadata_df: DataFrame содержащий в себе основные данные по отчетам. [id сектора на research,
+        id клиента на research, id клиента в нашей БД, отчет PL, балансовый отчет, отчет денежных движений]
+        """
+        ecom_tables = pd.read_html(page_html.text, decimal=',', thousands='.')
+        for table_num, table in enumerate(ecom_tables):
+            self._logger.info()
+            if table_num in [1, 2, 3]:
+                cleaned_columns = self.filter_list(table.columns.values)  # очистка от ненужных колонок
+                table = table[cleaned_columns].iloc[1:, :]  # отброс наименования таблицы
+                table.rename(columns={cleaned_columns[0]: ''}, inplace=True)
+                table[cleaned_columns[1:]] = table[cleaned_columns[1:]].replace('\.', ',', regex=True)
+                # запись полученных таблиц в исходных df
+                if table_num == 1:
+                    metadata_df.loc[metadata_df.company_id == company_id, 'pl'] = [table.to_dict()]
+                elif table_num == 2:
+                    metadata_df.loc[metadata_df.company_id == company_id, 'balance'] = [table.to_dict()]
+                elif table_num == 3:
+                    metadata_df.loc[metadata_df.company_id == company_id, 'money'] = [table.to_dict()]
+        return metadata_df
+
 
 class Newsletter:
     """Создает текста для рассылок"""
