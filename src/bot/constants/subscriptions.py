d4ef9e02e36ba4309ad5edb5f4817be0aa91a702
Gerfurmur <timur71398@gmail.com>
2024-04-05 12:41:39 +0300

    Перемещение quotes в папку
    Signed-off-by: Gerfurmur <timur71398@gmail.com>

--- src/bot/constants/subscriptions.py
+++ src/bot/constants/subscriptions.py
@@ -7,13 +7,18 @@ END_WRITE_SUBS = 'tg_end_write_subs'
 # ADD_ALL_SUBS_BY_DOMAIN = 'add_all_subs_by_domain'
 # ----------------------------------------------------
 # ------------TELEGRAM SUBSCRIPTIONS -----------------
-USER_TG_SUBS = 'tg_subs'
 TG_MENU = 'tg_subs_menu'
+TG_SECTION_MENU = 'tg_section_menu'
+
+USER_TG_SUBS = 'tg_subs'
 TG_CHANNEL_INFO = 'tg_channel_info'
+
 TG_SUBS_INDUSTRIES_MENU = 'tg_subs_industries_menu'
 INDUSTRY_TG_CHANNELS = 'industry_tg_channels'
+
 TG_SUBS_DELETE_ALL = 'tg_subs_delete_all'
 TG_SUBS_DELETE_ALL_DONE = 'tg_subs_delete_all_done'
+
 TG_SUB_ACTION = 'tg_sub_action'
 TG_END_WRITE_SUBS = 'tg_end_write_subs'
 # ----------------------------------------------------
