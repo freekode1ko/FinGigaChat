2e293e54eb698e728b77a07d7cc47416311e2c14
alinlpkv <alinlpkv@gmail.com>
2024-04-11 16:45:19 +0800

    проверка наличия почты у пользовтеля

--- src/bot/db/whitelist.py
+++ src/bot/db/whitelist.py
@@ -1,6 +1,7 @@
-from sqlalchemy import text
+from sqlalchemy import text, select
 
 from db.database import engine
+from db.models import Whitelist
 
 
 def update_user_email(user_id: int, user_email: str):
@@ -12,7 +13,14 @@ def update_user_email(user_id: int, user_email: str):
 
 
 def is_new_user_email(user_email: str) -> bool:
-    """Проверка на наличие почты в таблице"""
+    """Проверка на наличие почты в таблице (занята кем-то или нет)"""
     query = text('SELECT user_email FROM whitelist WHERE LOWER(user_email)=:user_email')
     with engine.connect() as conn:
         return not conn.execute(query.bindparams(user_email=user_email.lower())).scalar()
+
+
+def is_user_email_exist(user_id: int) -> bool:
+    """Проверка наличия почты у пользователя"""
+    query = select(Whitelist.user_email).where(Whitelist.user_id==user_id)
+    with engine.connect() as conn:
+        return conn.execute(query).scalar()
