292314bdf909dce82e42cf4cd0ba46064d39d913
Gerfurmur <timur71398@gmail.com>
2024-04-26 01:05:45 +0300

    Новые таблицы для меню продуктов. Миграции для таблиц с данными и путями к файлам
    Signed-off-by: Gerfurmur <timur71398@gmail.com>

--- src/bot/constants/products/product_shelf/const.py
+++ src/bot/constants/products/product_shelf/const.py
@@ -6,39 +6,3 @@ TITLE = 'Продуктовая полка'
 MENU = 'product_shelf_menu'
 
 GET_PRODUCT_PDF = 'get_product_pdf'
-
-
-DATA_ROOT_PATH = config.PATH_TO_SOURCES / 'products' / 'product_shelf'
-
-PRODUCT_SHELF_DATA = [
-    {
-        'name': 'Кредитование',
-        'message_title': 'Продукты кредитования',
-        'pdf_path': DATA_ROOT_PATH / 'Crediting',
-    },
-    {
-        'name': 'Пассивы',
-        'message_title': 'Продукты пассивов',
-        'pdf_path': DATA_ROOT_PATH / 'liabilities',
-    },
-    {
-        'name': 'GM',
-        'message_title': 'Продукты GM',
-        'pdf_path': DATA_ROOT_PATH / 'GM_products',
-    },
-    {
-        'name': 'ВЭД',
-        'message_title': 'Продукты ВЭД',
-        'pdf_path': DATA_ROOT_PATH / 'foreign_trade_activities',
-    },
-    {
-        'name': 'Экосистема',
-        'message_title': 'Продукты экосистемы',
-        'pdf_path': DATA_ROOT_PATH / 'ecosystem',
-    },
-    {
-        'name': 'Прочий НКД',
-        'message_title': 'Прочие продукты НКД',
-        'pdf_path': DATA_ROOT_PATH / 'accumulated_coupon_income',
-    },
-]
