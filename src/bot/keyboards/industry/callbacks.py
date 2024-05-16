9505e34960819d78cfb2e33cddc0427223d8e790
Gerfurmur <timur71398@gmail.com>
2024-05-16 21:33:46 +0300

    Переработка меню получения сводки новостей по тг каналам. Получение рассылки новостей тг каналов
    Signed-off-by: Gerfurmur <timur71398@gmail.com>

--- src/bot/keyboards/industry/callbacks.py
+++ src/bot/keyboards/industry/callbacks.py
@@ -4,11 +4,11 @@ from constants import industry as callback_prefixes
 
 
 class SelectNewsPeriod(CallbackData, prefix=callback_prefixes.SELECTED_INDUSTRY_TOKEN):
-    industry_id: int
+    section_id: int
     my_subscriptions: bool
 
 
 class GetNewsDaysCount(CallbackData, prefix=callback_prefixes.GET_INDUSTRY_TG_NEWS):
-    industry_id: int
+    section_id: int
     my_subscriptions: bool
     days_count: int
