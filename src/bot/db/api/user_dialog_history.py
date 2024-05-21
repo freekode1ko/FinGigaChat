feec2fc0fc8eff489f164b849fb85fe841ae99c3
alinlpkv <alinlpkv@gmail.com>
2024-05-21 16:07:28 +0300

    db methods

--- src/bot/db/api/user_dialog_history.py
+++ src/bot/db/api/user_dialog_history.py
@@ -1,3 +1,4 @@
+"""Методы по взаимодействию с таблицей UserDialogHistory в БД."""
 import sqlalchemy as sa
 
 from db import models
@@ -10,6 +11,18 @@ EMPTY_DIALOG: dict[str, list] = {DIALOG: []}
 
 class UserDialogHistoryCRUD(BaseCRUD[models.UserDialogHistory]):
     """Класс, который создает объекты для взаимодействия с таблицей models.UserDialogHistory."""
+
+    async def _update_dialog(self, user_id: int, dialog: dict[str, list]) -> None:
+        """
+        Обновление истории диалога.
+
+        :param user_id:         Telegram id пользователя.
+        :param dialog:          История диалога.
+        """
+        async with self._async_session_maker() as session:
+            stmt = sa.update(self._table).values(dialog=dialog).where(self._table.user_id == user_id)
+            await session.execute(stmt)
+            await session.commit()
 
     async def get_user_dialog(self, user_id: int) -> dict[str, list[dict]]:
         """
@@ -28,36 +41,39 @@ class UserDialogHistoryCRUD(BaseCRUD[models.UserDialogHistory]):
             return EMPTY_DIALOG
         return user_dialog_obj.dialog
 
-    async def update_dialog(self, user_id: int, dialog: dict[str, list]) -> None:
+    async def add_msgs_to_user_dialog(self, user_id: int, messages: dict[str, str], need_replace: bool = False) -> None:
         """
-        Обновление истории диалога.
-
-        :param user_id:         Telegram id пользователя.
-        :param dialog:          История диалога.
-        """
-        async with self._async_session_maker() as session:
-            stmt = sa.update(self._table).values(dialog=dialog).where(self._table.user_id == user_id)
-            await session.execute(stmt)
-            await session.commit()
-
-    async def add_msgs_to_user_dialog(self, user_id: int, messages: dict[str, str]) -> None:
-        """
-        Добавление сообщений в историю диалога между пользователем и ИИ.
+        Добавление или изменение сообщений в историю диалога между пользователем и ИИ.
 
         :param user_id:         Telegram id пользователя.
         :param messages:        Диалог из двух сообщений: пользователь и ИИ.
+        :param need_replace:     Необходимость замены последних сообщений новыми.
         """
         dialog = await self.get_user_dialog(user_id)
+        if need_replace and len(dialog[DIALOG]):
+            dialog[DIALOG].pop()
         dialog[DIALOG].append(messages)
-        await self.update_dialog(user_id, dialog)
+        await self._update_dialog(user_id, dialog)
 
     async def clear_user_dialog(self, user_id: int) -> None:
         """
         Очистка истории диалога между пользователем и ИИ.
 
-        :param user_id:         Telegram id пользователя.
+        :param user_id:      Telegram id пользователя.
         """
-        await self.update_dialog(user_id, EMPTY_DIALOG)
+        await self._update_dialog(user_id, EMPTY_DIALOG)
+
+    async def get_last_user_query(self, user_id: int) -> str:
+        """
+        Получение последнего пользовательского запроса.
+
+        :param user_id:     Telegram id пользователя.
+        :return:            Последний пользовательский запрос.
+        """
+        user_dialog_obj = await self.get(user_id)
+        if user_dialog_obj is None or not len(user_dialog_obj.dialog[DIALOG]):
+            return ''
+        return user_dialog_obj.dialog[DIALOG][-1]['user']
 
 
 user_dialog_history_db = UserDialogHistoryCRUD(models.UserDialogHistory, models.UserDialogHistory.user_id, logger)
--- src/bot/db/rag_user_feedback.py
+++ src/bot/db/rag_user_feedback.py
@@ -1,37 +1,70 @@
+"""Методы по взаимодействию с таблицей UserRagFeedback в БД."""
 import datetime
 
-from sqlalchemy import text
+from sqlalchemy import update
+from sqlalchemy.dialects.postgresql import insert as insert_pg
 
-from db.database import engine
 from constants.enums import RetrieverType
+from db.database import async_session
+from db.models import RAGUserFeedback
 
 
-def add_rag_activity(chat_id: int,
-                     bot_msg_id: int,
-                     retriever_type: RetrieverType,
-                     date: datetime,
-                     query: str,
-                     response: str):
-    """Логирование использования RAG-системы"""
-    sql_query = text(
-        'INSERT INTO rag_user_feedback (chat_id, bot_msg_id, retriever_type, date, query, response) '
-        'VALUES (:chat_id, :bot_msg_id, :retriever_type, :date, :query, :response)'
+async def add_rag_activity(
+        chat_id: int,
+        bot_msg_id: int,
+        retriever_type: RetrieverType,
+        date: datetime,
+        query: str,
+        rephrase_query: str,
+        response: str
+):
+    """
+    Логирование использования RAG-системы.
+
+    :param chat_id:         Id чата с пользователем.
+    :param bot_msg_id:      Id сообщения от бота.
+    :param retriever_type:  Тип ретривера.
+    :param date:            Дата+время ответа сообщения от бота.
+    :param query:           Запрос пользователя.
+    :param rephrase_query:  Перефразированный с помощью GigaChat запрос пользователя.
+    :param response:        Ответ ретривера (сообщение от бота).
+    """
+    stmt = insert_pg(RAGUserFeedback).values(
+        chat_id=chat_id,
+        bot_msg_id=bot_msg_id,
+        retriever_type=retriever_type.name,
+        date=date.replace(tzinfo=None),
+        query=query,
+        rephrase_query=rephrase_query,
+        response=response
     )
-    with engine.connect() as conn:
-        conn.execute(sql_query.bindparams(chat_id=chat_id,
-                                          bot_msg_id=bot_msg_id,
-                                          retriever_type=retriever_type.name,
-                                          date=date,
-                                          query=query,
-                                          response=response))
-        conn.commit()
+    stmt = stmt.on_conflict_do_update(
+        constraint="rag_user_feedback_pkey",
+        set_={
+            "chat_id": stmt.excluded.chat_id,
+            "bot_msg_id": stmt.excluded.bot_msg_id,
+            "retriever_type": retriever_type.name,
+            "response": response,
+        }
+    )
+    async with async_session() as session:
+        await session.execute(stmt)
+        await session.commit()
 
 
-def update_user_reaction(chat_id: int, bot_msg_id: int, reaction: bool):
-    """Добавление обратной связи от пользователя по использованию RAG-системы"""
-    sql_query = text('UPDATE rag_user_feedback SET reaction=:reaction WHERE chat_id=:chat_id AND bot_msg_id=:bot_msg_id')
-    with engine.connect() as conn:
-        conn.execute(sql_query.bindparams(chat_id=chat_id,
-                                          bot_msg_id=bot_msg_id,
-                                          reaction=reaction))
-        conn.commit()
+async def update_user_reaction(chat_id: int, bot_msg_id: int, reaction: bool):
+    """
+    Добавление обратной связи от пользователя по использованию RAG-системы.
+
+    :param chat_id:     Id чата с пользователем.
+    :param bot_msg_id:  Id сообщения от бота.
+    :param reaction:    Реакция пользователя на ответ RAG-системы (True/False).
+    """
+    stmt = (
+        update(RAGUserFeedback)
+        .where(RAGUserFeedback.chat_id == chat_id, RAGUserFeedback.bot_msg_id == bot_msg_id)
+        .values(reaction=reaction)
+    )
+    async with async_session() as session:
+        await session.execute(stmt)
+        await session.commit()
