3c9d9b65007a6657b9c576e4a009dcb59101d669
alinlpkv <alinlpkv@gmail.com>
2024-05-22 09:20:18 +0300

    giga

--- src/bot/handlers/ai/gigachat/gigachat.py
+++ src/bot/handlers/ai/gigachat/gigachat.py
@@ -1,12 +1,13 @@
+"""Обработка общения пользователя с GigaChat."""
 from aiogram import types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
-import module.gigachat as gig
 from constants.constants import giga_ans_footer
 from handlers.ai.handler import router
 from log.bot_logger import logger, user_logger
+import module.gigachat as gig
 from utils.base import user_in_whitelist
 
 
@@ -14,6 +15,8 @@ chat = gig.GigaChat(logger)
 
 
 class GigaChatState(StatesGroup):
+    """Автомат состояний общения с GigaChat."""
+
     gigachat_mode = State()
     gigachat_query = State()
 
@@ -21,10 +24,10 @@ class GigaChatState(StatesGroup):
 @router.message(Command('gigachat'))
 async def set_gigachat_mode(message: types.Message, state: FSMContext) -> None:
     """
-    Переключение в режим общения с Gigachat
+    Переключение в режим общения с Gigachat.
 
-    :param message: Объект, содержащий в себе информацию по отправителю, чату и сообщению
-    :param state: Объект, который хранит состояние FSM для пользователя
+    :param message:     Объект, содержащий в себе информацию по отправителю, чату и сообщению.
+    :param state:       Объект, который хранит состояние FSM для пользователя.
     """
     chat_id, full_name, user_msg = message.chat.id, message.from_user.full_name, message.text
 
@@ -57,27 +60,41 @@ async def set_gigachat_mode(message: types.Message, state: FSMContext) -> None:
 
 @router.message(GigaChatState.gigachat_mode)
 async def handler_gigachat_mode(message: types.Message) -> None:
-    """Отправка пользователю ответа, сформированного Gigachat, на сообщение пользователя"""
+    """Отправка пользователю ответа, сформированного Gigachat, на сообщение пользователя."""
     await ask_giga_chat(message)
+
+
+def _get_response(chat_id: int, full_name: str, query: str) -> str:
+    """
+    Получение и форматирование ответа от GigaChat.
+
+    :param chat_id:     Telegram id пользователя.
+    :param full_name:   Полное имя пользователя.
+    :param query:       Запрос пользователя (перефразированный с помощью GigaChat)
+    :return:            Отформатированный ответ на запрос.
+    """
+    try:
+        giga_answer = chat.get_giga_answer(text=query)
+        user_logger.info(f'*{chat_id}* {full_name} - "{query}" : На запрос GigaChat ответил: "{giga_answer}"')
+        response = f'{giga_answer}\n\n{giga_ans_footer}'
+    except Exception as e:
+        logger.critical(f'ERROR : GigaChat не сформировал ответ по причине: {e}"')
+        user_logger.critical(f'*{chat_id}* {full_name} - "{query}" : GigaChat не сформировал ответ по причине: {e}"')
+        response = 'Извините, я пока не могу ответить на ваш запрос'
+
+    return response
 
 
 async def ask_giga_chat(message: types.Message, first_user_query: str = '') -> None:
     """
-    Отправляет ответ на запрос пользователя
-    :param message: Message от пользователя
-    :param first_user_query: запрос от пользователя вне режима gigachat
+    Отправляет ответ на запрос пользователя.
+
+    :param message:             Message от пользователя.
+    :param first_user_query:    Запрос от пользователя вне режима GigaChat.
     """
-    user_msg = first_user_query if first_user_query else message.text
+    user_query = first_user_query if first_user_query else message.text
     chat_id, full_name = message.chat.id, message.from_user.full_name
     await message.bot.send_chat_action(message.chat.id, 'typing')
 
-    try:
-        giga_answer = chat.get_giga_answer(text=user_msg)
-        user_logger.info(f'*{chat_id}* {full_name} - "{user_msg}" : На запрос GigaChat ответил: "{giga_answer}"')
-        response = f'{giga_answer}\n\n{giga_ans_footer}'
-    except Exception as e:
-        logger.critical(f'ERROR : GigaChat не сформировал ответ по причине: {e}"')
-        user_logger.critical(f'*{chat_id}* {full_name} - "{user_msg}" : GigaChat не сформировал ответ по причине: {e}"')
-        response = 'Извините, я пока не могу ответить на ваш запрос'
-
-    await message.answer(response,  parse_mode='HTML', disable_web_page_preview=True)
+    response = _get_response(chat_id, full_name, user_query)
+    await message.answer(response, parse_mode='HTML', disable_web_page_preview=True)
