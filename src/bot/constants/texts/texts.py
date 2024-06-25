b6f1bff148352c0037bf6bb4eb080826145ed36c
Alex Gavrilov <fdhbkjd@mail.ru>
2024-06-25 14:13:31 +0300

    Remake structure

---
+++ src/bot/constants/texts/texts.py
@@ -0,0 +1,23 @@
+"""Клас для хранения текстовок """
+from pydantic_settings import BaseSettings
+
+
+class AllListOfTexts(BaseSettings):
+    """
+    Класс для хранения текстовок, которы будут загружены в редис
+
+    Examples:
+    top_news_count: str = Field(3, env='TOP_NEWS_COUNT', default="1")
+    DB_HOST: str = "123"
+    DB_PORT: int = 123
+    """
+
+    # CumReports
+    pass
+
+
+class CallReportsTexts(BaseSettings):
+    """Класс для хранения текстовок кол репортов"""
+
+    CALL_REPORTS_MAIN_MENU: str = 'Вы вызвали функцию работы с протоколами встреч, что вы хотите сделать?'
+    CALL_REPORTS_CLOSE: str = 'Меню протоколов встреч закрыто.'
