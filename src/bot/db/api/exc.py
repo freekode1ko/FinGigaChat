f8c08dcb6cdf3ddae94b9ce217170c0e507cf0b6
Gerfurmur <timur71398@gmail.com>
2024-06-28 11:13:46 +0300

    Правка док строк
    Signed-off-by: Gerfurmur <timur71398@gmail.com>

--- src/bot/db/api/exc.py
+++ src/bot/db/api/exc.py
@@ -1,4 +1,4 @@
-"""Запросы к бд связанные с продуктами"""
+"""Запросы к бд связанные с курсами валют"""
 import sqlalchemy as sa
 from sqlalchemy.orm import joinedload
 
@@ -8,17 +8,17 @@ from log.bot_logger import logger
 
 
 class ExcCRUD(BaseCRUD[models.Exc]):
-    """Класс, который создает объекты для взаимодействия с таблицей models.Product"""
+    """Класс, который создает объекты для взаимодействия с таблицей models.Exc"""
 
     def __init__(self) -> None:
-        """Инициализировать интерфейс ProductCRUD"""
+        """Инициализировать интерфейс ExcCRUD"""
         super().__init__(models.Exc, models.Exc.display_order, logger)
 
     def _get(self) -> sa.Select:
         """
-        Получить запрос для выгрузки продуктов с подгрузкой подкатегорий, родительской категории, связанных документов
+        Получить запрос для выгрузки курсов валют с подгрузкой тип курсов валют, связанного источника данных
 
-        :returns: Возвращает запрос для выгрузки продуктов
+        :returns: Возвращает запрос для выгрузки курсов валют
         """
         stmt = (
             sa.select(self._table)
@@ -32,9 +32,9 @@ class ExcCRUD(BaseCRUD[models.Exc]):
 
     async def get(self, id_: int) -> models.Exc | None:
         """
-        Получить продукт по его ID
+        Получить курс по его ID
 
-        :param id_: ID продукта
+        :param id_: ID курса
         :returns: Возвращает либо сам объект, который есть в БД, либо None
         """
         async with self._async_session_maker() as session:
@@ -43,9 +43,9 @@ class ExcCRUD(BaseCRUD[models.Exc]):
 
     async def get_all(self) -> list[models.Exc]:
         """
-        Получить корневой продукт
+        Получить все курсы валют
 
-        :returns: Возвращает либо сам объект, который есть в БД, либо None
+        :returns: Список курсов
         """
         async with self._async_session_maker() as session:
             result = await session.scalars(self._get())
