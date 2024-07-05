47daed32a15428c2d7b893a913b091ab01a5d7c1
Grigory Yolkin <gyolkin380@yandex.ru>
2024-07-05 14:41:49 +0300

    added simple static & template for quotes page

---
+++ src/web_app/frontend/static/js/script.js
@@ -0,0 +1,72 @@
+const API_URL = "https://127.0.0.1:443/";
+const QUOTES_PER_PAGE = 10;
+let LOADED_QUOTES = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  lucide.createIcons();
+
+  let quotesData;
+  fetch(API_URL.concat("quotation/RUB"))
+    .then((response) => response.json())
+    .then((data) => {
+      quotesData = data;
+      renderQuotesData(quotesData);
+    })
+    .catch((err) => console.error("Котировки не были загружены", err));
+
+  fetch(API_URL.concat("news/"))
+    .then((response) => response.json())
+    .then((data) => renderNewsData(data.news))
+    .catch((err) => console.error("Новости не были загружены", err));
+
+  const showQuotesButton = document.getElementById("load-quotes");
+  showQuotesButton.addEventListener("click", () => {
+    renderQuotesData(quotesData);
+    if (LOADED_QUOTES >= Object.keys(quotesData).length) {
+      showQuotesButton.style.display = "none";
+    }
+  });
+});
+
+function renderQuotesData(quotesData) {
+  const quotesElement = document.getElementById("quotes");
+  const quotesKeys = Object.keys(quotesData);
+  const quotesToRender = quotesKeys.slice(
+    LOADED_QUOTES,
+    LOADED_QUOTES + QUOTES_PER_PAGE
+  );
+  quotesToRender.forEach((quoteKey) => {
+    const quoteCardHTML = createQuoteCard(quoteKey, quotesData[quoteKey]);
+    quotesElement.innerHTML += quoteCardHTML;
+  });
+  LOADED_QUOTES += quotesToRender.length;
+}
+
+function renderNewsData(newsData) {
+  const newsElement = document.getElementById("news");
+  newsData.forEach((news) => {
+    const newsCardHTML = createShortNewsCard(news.title, news.text);
+    newsElement.innerHTML += newsCardHTML;
+  });
+}
+
+function createShortNewsCard(title, text) {
+  return `
+    <div class="news-item">
+        <h2 class="news-title">${title}</h2>
+        <p class="news-description truncate-4">${text}</p>
+    </div>
+  `;
+}
+
+function createQuoteCard(key, value) {
+  return `
+    <div class="quote-item">
+      <p>RUB/${key}</p>
+      <div class="quote-value">
+          <p>${value}</p>
+          <!-- <p class="quote-change negative">-0.19%</p> -->
+      </div>
+    </div>
+  `;
+}
