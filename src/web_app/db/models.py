2c801c39eca0f7b458d0d035beddc415e3d322d1
Alex Gavrilov <fdhbkjd@mail.ru>
2024-07-16 11:31:58 +0300

    fix review

--- src/web_app/db/models.py
+++ src/web_app/db/models.py
@@ -1,18 +1,5 @@
 import sqlalchemy as sa
-from sqlalchemy import (
-    BigInteger,
-    Column,
-    DateTime,
-    ForeignKey,
-    Integer,
-    Text,
-    String,
-    ARRAY,
-    JSON,
-    Identity,
-
-)
-from sqlalchemy.orm import declarative_base, relationship, deferred
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -21,37 +8,37 @@ metadata = Base.metadata
 class Whitelist(Base):
     __tablename__ = 'whitelist'
 
-    user_id = Column(BigInteger, primary_key=True)
-    username = Column(Text)
-    full_name = Column(Text)
-    user_type = Column(Text)
-    user_status = Column(Text)
-    user_email = Column(Text, server_default=sa.text("''::text"))
-    subscriptions = Column(Text)
+    user_id = sa.Column(sa.BigInteger, primary_key=True)
+    username = sa.Column(sa.Text)
+    full_name = sa.Column(sa.Text)
+    user_type = sa.Column(sa.Text)
+    user_status = sa.Column(sa.Text)
+    user_email = sa.Column(sa.Text, server_default=sa.text("''::text"))
+    subscriptions = sa.Column(sa.Text)
 
 
 class UserMeeting(Base):
     __tablename__ = 'user_meeting'
     __table_args__ = {'comment': 'Перечень встреч пользователей'}
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(ForeignKey('whitelist.user_id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
-    theme = Column(Text, nullable=False, default='Напоминание', comment='Тема встречи')
-    date_create = Column(DateTime, comment='Время создания встречи (UTC)')
-    date_start = Column(DateTime, nullable=False, comment='Время начала встречи (UTC)')
-    date_end = Column(DateTime, nullable=False, comment='Время окончания встречи (UTC)')
-    timezone = Column(Integer, comment='Таймзона пользователя во время использования web app', nullable=False)
-    notify_count = Column(Integer, server_default=sa.text('0'), comment='Количество отправленных напоминаний')
-    description = Column(Text, comment='Описание встречи')
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    user_id = sa.Column(sa.ForeignKey('whitelist.user_id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    theme = sa.Column(sa.Text, nullable=False, default='Напоминание', comment='Тема встречи')
+    date_create = sa.Column(sa.DateTime, comment='Время создания встречи (UTC)')
+    date_start = sa.Column(sa.DateTime, nullable=False, comment='Время начала встречи (UTC)')
+    date_end = sa.Column(sa.DateTime, nullable=False, comment='Время окончания встречи (UTC)')
+    timezone = sa.Column(sa.Integer, comment='Таймзона пользователя во время использования web app', nullable=False)
+    notify_count = sa.Column(sa.Integer, server_default=sa.Text('0'), comment='Количество отправленных напоминаний')
+    description = sa.Column(sa.Text, comment='Описание встречи')
 
 
 class SourceGroup(Base):
     __tablename__ = 'source_group'
     __table_args__ = {'comment': 'Справочник выделенных подгрупп среди источников'}
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(64), nullable=False)
-    name_latin = Column(String(64), nullable=False)
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(64), nullable=False)
+    name_latin = sa.Column(sa.String(64), nullable=False)
 
     parser_source = relationship('ParserSource', back_populates='source_group')
 
@@ -60,16 +47,16 @@ class ParserSource(Base):
     __tablename__ = 'parser_source'
     __table_args__ = {'comment': 'Справочник источников'}
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    alt_names = Column(ARRAY(Text), nullable=False)
-    response_format = Column(Text)
-    source = Column(Text, nullable=False)
-    source_group_id = Column(ForeignKey('source_group.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
-    last_update_datetime = Column(DateTime)
-    previous_update_datetime = Column(DateTime)
-    params = Column(JSON)
-    before_link = Column(Text, nullable=True, server_default='')
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(255), nullable=False)
+    alt_names = sa.Column(sa.ARRAY(sa.Text), nullable=False)
+    response_format = sa.Column(sa.Text)
+    source = sa.Column(sa.Text, nullable=False)
+    source_group_id = sa.Column(sa.ForeignKey('source_group.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
+    last_update_datetime = sa.Column(sa.DateTime)
+    previous_update_datetime = sa.Column(sa.DateTime)
+    params = sa.Column(sa.JSON)
+    before_link = sa.Column(sa.Text, nullable=True, server_default='')
 
     source_group = relationship('SourceGroup', back_populates='parser_source')
 
@@ -77,10 +64,10 @@ class ExcType(Base):
     __tablename__ = 'bot_exc_type'
     __table_args__ = {'comment': 'Справочник типов курсов валют'}
 
-    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, comment='id типа')
-    name = sa.Column(sa.String(64), nullable=False, comment='Наименование типа курсов валют')
-    description = sa.Column(sa.Text(), nullable=True, server_default=sa.text("''::text"), comment='Описание')
-    display_order = sa.Column(sa.Integer(), server_default=sa.text('0'), nullable=False, comment='Порядок отображения')
+    id = sa.sa.Column(sa.sa.Integer, primary_key=True, autoincrement=True, comment='id типа')
+    name = sa.sa.Column(sa.sa.String(64), nullable=False, comment='Наименование типа курсов валют')
+    description = sa.sa.Column(sa.Text(), nullable=True, server_default=sa.text("''::text"), comment='Описание')
+    display_order = sa.sa.Column(sa.Integer(), server_default=sa.text('0'), nullable=False, comment='Порядок отображения')
 
 class Exc(Base):
     __tablename__ = 'exc'
@@ -103,13 +90,13 @@ class Exc(Base):
 class Article(Base):
     __tablename__ = 'article'
 
-    id = Column(Integer, Identity(always=True, start=1, increment=1, minvalue=1,
+    id = sa.Column(sa.Integer, sa.Identity(always=True, start=1, increment=1, minvalue=1,
                 maxvalue=2147483647, cycle=False, cache=1), primary_key=True)
-    link = Column(Text, nullable=False, comment='Ссылка на новость')
-    date = Column(DateTime, nullable=False, comment='Дата и время публикации новости')
-    text_ = Column('text', Text, nullable=False, comment='Исходный текст новости')
-    title = Column(Text, comment='Заголовок новости (если заголовка не было, то его формирует гигачат)')
-    text_sum = Column(Text, comment='Сформированная гигачатом сводка по новости')
+    link = sa.Column(sa.Text, nullable=False, comment='Ссылка на новость')
+    date = sa.Column(sa.DateTime, nullable=False, comment='Дата и время публикации новости')
+    text_ = sa.Column('text', sa.Text, nullable=False, comment='Исходный текст новости')
+    title = sa.Column(sa.Text, comment='Заголовок новости (если заголовка не было, то его формирует гигачат)')
+    text_sum = sa.Column(sa.Text, comment='Сформированная гигачатом сводка по новости')
 
     relation_client_article = relationship('RelationClientArticle', back_populates='article')
 
@@ -117,10 +104,10 @@ class Article(Base):
 class RelationClientArticle(Base):
     __tablename__ = 'relation_client_article'
 
-    client_id = Column(ForeignKey('client.id', ondelete='CASCADE', onupdate='CASCADE'),
+    client_id = sa.Column(sa.ForeignKey('client.id', ondelete='CASCADE', onupdate='CASCADE'),
                        primary_key=True, nullable=False)
-    article_id = Column(ForeignKey('article.id', ondelete='CASCADE', onupdate='CASCADE'),
+    article_id = sa.Column(sa.ForeignKey('article.id', ondelete='CASCADE', onupdate='CASCADE'),
                         primary_key=True, nullable=False)
-    client_score = Column(Integer)
+    client_score = sa.Column(sa.Integer)
 
     article = relationship('Article', back_populates='relation_client_article')
