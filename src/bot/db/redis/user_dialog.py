807fd02ec50506f0b530ac0b90bc510d9bf914c8
alinlpkv <alinlpkv@gmail.com>
2024-07-22 11:37:59 +0300

    добавление истор запроса в редис

--- src/bot/db/redis/user_dialog.py
+++ src/bot/db/redis/user_dialog.py
@@ -1,9 +1,34 @@
-"""Обработка истории диалога между пользователем и ботом в Redis."""
+"""Обработка исторического запроса и истории диалога между пользователем и ботом в Redis."""
 
 import json
 
 from constants.constants import COUNT_OF_USEFUL_LAST_MSGS, KEEP_DIALOG_TIME
 from db.redis.client import redis_client
+
+HISTORY_QUERY_KEY = 'history_query_{user_id}'
+
+
+async def get_history_query(user_id: int) -> str:
+    """
+    Получение исторически перефразированного запроса пользователя.
+
+    :param user_id: Telegram id пользователя.
+    :return:        Исторически перефразированный запрос пользователя.
+    """
+    history_query = await redis_client.get(HISTORY_QUERY_KEY.format(user_id=user_id))
+    return history_query if history_query else ''
+
+
+async def update_history_query(user_id: int, history_query: str) -> None:
+    """
+    Обновление исторически перефразированного запроса пользователя.
+
+    :param user_id:         Telegram id пользователя.
+    :param history_query:   Исторически перефразированный запрос пользователя.
+    """
+    key = HISTORY_QUERY_KEY.format(user_id=user_id)
+    await redis_client.set(name=key, value=history_query)
+    await redis_client.expire(name=key, time=KEEP_DIALOG_TIME)
 
 
 async def get_dialog(user_id: int) -> list[dict[str, str]]:
@@ -37,13 +62,14 @@ async def update_dialog(user_id: int, msgs: dict[str, str], need_replace: bool =
     await redis_client.expire(name=str(user_id), time=KEEP_DIALOG_TIME)
 
 
-async def del_dialog(user_id: int) -> None:
+async def del_dialog_and_history_query(user_id: int) -> None:
     """
-    Очистка истории диалога между пользователем и ИИ.
+    Очистка исторического запроса пользователя и истории диалога между пользователем и ИИ.
 
     :param user_id:      Telegram id пользователя.
     """
     await redis_client.delete(str(user_id))
+    await redis_client.delete(HISTORY_QUERY_KEY.format(user_id=user_id))
 
 
 async def get_last_user_msg(user_id: int) -> str:
