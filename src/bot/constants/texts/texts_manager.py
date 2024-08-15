055ea8de914e217e0a16a29083372fe2d4eee622
alinlpkv <alinlpkv@gmail.com>
2024-08-15 16:34:17 +0300

    проверка на совпадение констант

--- src/bot/constants/texts/texts_manager.py
+++ src/bot/constants/texts/texts_manager.py
@@ -5,6 +5,7 @@ from db.redis.client import redis_client_sync as redis_client
 
 class TextsManager:
     """Класс для хранения конфига в редисе"""
+    PATTERN = 'settings_'
 
     def __init__(self):
         self.__configs = [x() for x in CONFIG_CLASSES]
@@ -14,11 +15,17 @@ class TextsManager:
         """Загрузка переменных в redis"""
         for config_class in self.__configs:
             for key, value in config_class.dict().items():
-                redis_client.set(f'settings_{key}', value)
+                redis_value = redis_client.get(f'{self.PATTERN}{key}')
+                if redis_value == str(value):
+                    redis_client.set(f'{self.PATTERN}{key}', value)
+                else:
+                    raise ValueError(
+                        f'Значение "{value}" для ключа "{key}" не совпадает со значением "{redis_value}" в Redis.'
+                    )
 
     def __getattr__(self, item):
         """Получение атрибута из редиса"""
-        value = redis_client.get(f'settings_{item}')
+        value = redis_client.get(f'{self.PATTERN}{item}')
         if value is not None:
             return value
         for config in self.__configs:
