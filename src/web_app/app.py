9e95baccb01c10864d8917257685b38391dce857
Alex Gavrilov <fdhbkjd@mail.ru>
2024-09-09 19:17:07 +0300

    fix CI 56

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -14,21 +14,27 @@ import utils
 from db.meeting import get_user_meetings, add_meeting, get_user_email
 from log.logger_base import selector_logger
 from api.router import router as api_router
+from constants import constants
 from utils.decorators import handle_jinja_template_exceptions
 from utils.templates import templates
 
 
 logger = selector_logger(config.LOG_FILE, config.LOG_LEVEL)
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     await utils.add_notify_job(logger)
-#     utils.scheduler.start()
-#     yield
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await utils.add_notify_job(logger)
+    utils.scheduler.start()
+    yield
 
-app = FastAPI()#lifespan=lifespan)
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router, prefix="/api")
-app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+print(constants.PROJECT_DIR / 'frontend' / 'static')
+app.mount(
+    '/static',
+    StaticFiles(directory=str(constants.PROJECT_DIR / 'frontend' / 'static')),
+    name="static"
+)
 
 app.add_middleware(
     CORSMiddleware,
