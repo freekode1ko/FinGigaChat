a0e8a03c81fb79ff0ab20aed1e4416aef9a79263
Alex Gavrilov <fdhbkjd@mail.ru>
2024-10-14 14:43:39 +0300

    Fix data folder in migrations by changing the gitignore file

---
+++ src/projects/bot/migrations/data/cib_research_subs/research_types.py
@@ -0,0 +1,381 @@
+
+research_types = [
+    {
+        "name": "Экономика",
+        "description": "Раздел Экономика",
+        "research_section_id": 1,
+        "source_id": 19
+    },
+    {
+        "name": "Сырьевые товары",
+        "description": "Раздел Сырьевые товары",
+        "research_section_id": 2,
+        "source_id": 20
+    },
+    {
+        "name": "Ежедневные обзоры",
+        "description": "Раздел Валютный рыноки процентные ставки",
+        "research_section_id": 4,
+        "source_id": 21
+    },
+    {
+        "name": "Еженедельный обзор по процентным ставкам",
+        "description": "Раздел Валютный рыноки процентные ставки",
+        "research_section_id": 4,
+        "source_id": 22
+    },
+    {
+        "name": "Ежемесячный обзор по мягким валютам",
+        "description": "Раздел Валютный рыноки процентные ставки",
+        "research_section_id": 4,
+        "source_id": 23
+    },
+    {
+        "name": "Ежемесячный обзор по юаню",
+        "description": "Раздел Валютный рыноки процентные ставки",
+        "research_section_id": 4,
+        "source_id": 24
+    },
+    {
+        "name": "Прогноз валютного рынка на месяц",
+        "description": "Раздел Валютный рыноки процентные ставки",
+        "research_section_id": 4,
+        "source_id": 25
+    },
+    {
+        "name": "Прогноз итогов заседания ЦБ РФ",
+        "description": "Раздел Валютный рыноки процентные ставки",
+        "research_section_id": 4,
+        "source_id": 26
+    },
+    {
+        "name": "Обзор итогов заседания ЦБ",
+        "description": "Раздел Валютный рыноки процентные ставки",
+        "research_section_id": 4,
+        "source_id": 27
+    },
+    {
+        "name": "Долговые рынки сегодня",
+        "description": "Раздел Рынок долга",
+        "research_section_id": 3,
+        "source_id": 28
+    },
+    {
+        "name": "Итоги аукционов ОФЗ",
+        "description": "Раздел Рынок долга",
+        "research_section_id": 3,
+        "source_id": 29
+    },
+    {
+        "name": "Обзорв выплат по облигациям",
+        "description": "Раздел Рынок долга",
+        "research_section_id": 3,
+        "source_id": 30
+    },
+    {
+        "name": "Нефть и газ",
+        "description": "Раздел Отрасли",
+        "research_section_id": 5,
+        "source_id": 31
+    },
+    {
+        "name": "Промышленность",
+        "description": "Раздел Отрасли",
+        "research_section_id": 5,
+        "source_id": 32
+    },
+    {
+        "name": "Финансовый сектор",
+        "description": "Раздел Отрасли",
+        "research_section_id": 5,
+        "source_id": 33
+    },
+    {
+        "name": "энергетика",
+        "description": "Раздел Отрасли",
+        "research_section_id": 5,
+        "source_id": 34
+    },
+    {
+        "name": "торговля",
+        "description": "Раздел Отрасли",
+        "research_section_id": 5,
+        "source_id": 35
+    },
+    {
+        "name": "Недвижимость",
+        "description": "Раздел Отрасли",
+        "research_section_id": 5,
+        "source_id": 36
+    },
+    {
+        "name": "Транспорт",
+        "description": "Раздел Отрасли",
+        "research_section_id": 5,
+        "source_id": 37
+    },
+    {
+        "name": "Металлургия",
+        "description": "Раздел Отрасли",
+        "research_section_id": 5,
+        "source_id": 38
+    },
+    {
+        "name": "Газпром",
+        "description": "Раздел нефть и газ",
+        "research_section_id": 6,
+        "source_id": 39
+    },
+    {
+        "name": "Газпром нефть",
+        "description": "Раздел нефть и газ",
+        "research_section_id": 6,
+        "source_id": 40
+    },
+    {
+        "name": "Лукойл",
+        "description": "Раздел нефть и газ",
+        "research_section_id": 6,
+        "source_id": 41
+    },
+    {
+        "name": "НОВАТЭК",
+        "description": "Раздел нефть и газ",
+        "research_section_id": 6,
+        "source_id": 42
+    },
+    {
+        "name": "Роснефть",
+        "description": "Раздел нефть и газ",
+        "research_section_id": 6,
+        "source_id": 43
+    },
+    {
+        "name": "Татнефть",
+        "description": "Раздел нефть и газ",
+        "research_section_id": 6,
+        "source_id": 44
+    },
+    {
+        "name": "Транснефть",
+        "description": "Раздел нефть и газ",
+        "research_section_id": 6,
+        "source_id": 45
+    },
+    {
+        "name": "ММК",
+        "description": "Раздел Металургия",
+        "research_section_id": 13,
+        "source_id": 46
+    },
+    {
+        "name": "НЛМК",
+        "description": "Раздел Металургия",
+        "research_section_id": 13,
+        "source_id": 47
+    },
+    {
+        "name": "Норильский никель",
+        "description": "Раздел Металургия",
+        "research_section_id": 13,
+        "source_id": 48
+    },
+    {
+        "name": "Полюс",
+        "description": "Раздел Металургия",
+        "research_section_id": 13,
+        "source_id": 49
+    },
+    {
+        "name": "РУСАЛ",
+        "description": "Раздел Металургия",
+        "research_section_id": 13,
+        "source_id": 50
+    },
+    {
+        "name": "Северсталь",
+        "description": "Раздел Металургия",
+        "research_section_id": 13,
+        "source_id": 51
+    },
+    {
+        "name": "Акрон",
+        "description": "Раздел Промышленность",
+        "research_section_id": 7,
+        "source_id": 52
+    },
+    {
+        "name": "ФосАгро",
+        "description": "Раздел Промышленность",
+        "research_section_id": 7,
+        "source_id": 53
+    },
+    {
+        "name": "Банк \"Санкт-Петербург\"",
+        "description": "Раздел Финансовый сектор",
+        "research_section_id": 8,
+        "source_id": 54
+    },
+    {
+        "name": "ВТБ",
+        "description": "Раздел Финансовый сектор",
+        "research_section_id": 8,
+        "source_id": 55
+    },
+    {
+        "name": "Московская биржа",
+        "description": "Раздел Финансовый сектор",
+        "research_section_id": 8,
+        "source_id": 56
+    },
+    {
+        "name": "Совкомбанк",
+        "description": "Раздел Финансовый сектор",
+        "research_section_id": 8,
+        "source_id": 57
+    },
+    {
+        "name": "ТКС Холдинг",
+        "description": "Раздел Финансовый сектор",
+        "research_section_id": 8,
+        "source_id": 58
+    },
+    {
+        "name": "Интер РАО",
+        "description": "Раздел энергетика",
+        "research_section_id": 9,
+        "source_id": 59
+    },
+    {
+        "name": "Мосэнерго",
+        "description": "Раздел энергетика",
+        "research_section_id": 9,
+        "source_id": 60
+    },
+    {
+        "name": "ОГК-2",
+        "description": "Раздел энергетика",
+        "research_section_id": 9,
+        "source_id": 61
+    },
+    {
+        "name": "РусГидро",
+        "description": "Раздел энергетика",
+        "research_section_id": 9,
+        "source_id": 62
+    },
+    {
+        "name": "ТГК-1",
+        "description": "Раздел энергетика",
+        "research_section_id": 9,
+        "source_id": 63
+    },
+    {
+        "name": "ЭЛ5-Энерго",
+        "description": "Раздел энергетика",
+        "research_section_id": 9,
+        "source_id": 64
+    },
+    {
+        "name": "Юнипро",
+        "description": "Раздел энергетика",
+        "research_section_id": 9,
+        "source_id": 65
+    },
+    {
+        "name": "Fix Price Group",
+        "description": "Раздел торговля",
+        "research_section_id": 10,
+        "source_id": 66
+    },
+    {
+        "name": "Henderson",
+        "description": "Раздел торговля",
+        "research_section_id": 10,
+        "source_id": 67
+    },
+    {
+        "name": "X5 retail group",
+        "description": "Раздел торговля",
+        "research_section_id": 10,
+        "source_id": 68
+    },
+    {
+        "name": "Детский мир",
+        "description": "Раздел торговля",
+        "research_section_id": 10,
+        "source_id": 69
+    },
+    {
+        "name": "Лента",
+        "description": "Раздел торговля",
+        "research_section_id": 10,
+        "source_id": 70
+    },
+    {
+        "name": "Магнит",
+        "description": "Раздел торговля",
+        "research_section_id": 10,
+        "source_id": 71
+    },
+    {
+        "name": "М.Видео",
+        "description": "Раздел торговля",
+        "research_section_id": 10,
+        "source_id": 72
+    },
+    {
+        "name": "О'КЕЙ",
+        "description": "Раздел торговля",
+        "research_section_id": 10,
+        "source_id": 73
+    },
+    {
+        "name": "Русагро",
+        "description": "Раздел торговля",
+        "research_section_id": 10,
+        "source_id": 74
+    },
+    {
+        "name": "ГК ПИК",
+        "description": "Раздел Недвижимость",
+        "research_section_id": 11,
+        "source_id": 75
+    },
+    {
+        "name": "ГК Самолет",
+        "description": "Раздел Недвижимость",
+        "research_section_id": 11,
+        "source_id": 76
+    },
+    {
+        "name": "Эталон",
+        "description": "Раздел Недвижимость",
+        "research_section_id": 11,
+        "source_id": 77
+    },
+    {
+        "name": "ЛСР",
+        "description": "Раздел Недвижимость",
+        "research_section_id": 11,
+        "source_id": 78
+    },
+    {
+        "name": "Аэрофлот",
+        "description": "Раздел Транспорт",
+        "research_section_id": 12,
+        "source_id": 79
+    },
+    {
+        "name": "Глобалтранс",
+        "description": "Раздел Транспорт",
+        "research_section_id": 12,
+        "source_id": 80
+    },
+    {
+        "name": "Совкомфлот",
+        "description": "Раздел Транспорт",
+        "research_section_id": 12,
+        "source_id": 81
+    }
+]
---
+++ src/projects/bot/migrations/data/new_exchange_sources/exc_types.py
@@ -0,0 +1,20 @@
+"""Типы курсов валют"""
+
+data = [
+    {
+        'name': 'Spot',
+        'display_order': 0,
+    },
+    {
+        'name': 'Курс ЦБ',
+        'display_order': 1,
+    },
+    {
+        'name': 'Фьючерс (ближ.)',
+        'display_order': 2,
+    },
+    {
+        'name': 'Прочие',
+        'display_order': 3,
+    },
+]
