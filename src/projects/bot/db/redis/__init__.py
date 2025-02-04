1d3b6e4a31f80f35dcf0e8dc94747b408b1044e0
alinlpkv <alinlpkv@gmail.com>
2025-02-04 10:13:04 +0300

    fix(bot): update redis methods

--- src/projects/bot/db/redis/__init__.py
+++ src/projects/bot/db/redis/__init__.py
@@ -1,8 +1,12 @@
 """Пакет по работе с Redis."""
-
-from db.redis.last_activity import (
-    get_last_activity,
-    update_last_activity,
+from db.redis.client import redis_client
+from db.redis.user_constants import (
+    ACTIVITY_NAME,
+    del_user_constant,
+    FON_TASK_NAME,
+    FON_TASK_PATTERN,
+    get_user_constant,
+    update_user_constant,
 )
 from db.redis.user_dialog import (
     del_dialog_and_history_query,
@@ -14,12 +18,17 @@ from db.redis.user_dialog import (
 )
 
 __all__ = [
+    'redis_client',
+    'FON_TASK_NAME',
+    'FON_TASK_PATTERN',
+    'ACTIVITY_NAME',
+    'del_user_constant',
     'del_dialog_and_history_query',
     'get_dialog',
     'get_last_user_msg',
     'update_dialog',
-    'get_last_activity',
-    'update_last_activity',
+    'get_user_constant',
+    'update_user_constant',
     'get_history_query',
     'update_history_query',
 ]
---
+++ src/projects/bot/db/redis/user_constants.py
@@ -0,0 +1,42 @@
+"""Обработка времени последнего сообщения и фоновой задачи в Redis."""
+from db.redis.client import redis_client
+
+ACTIVITY_NAME = 'activity_{user_id}'
+FON_TASK_NAME = 'fon_task_{user_id}'
+FON_TASK_PATTERN = 'fon_task_'
+
+
+async def get_user_constant(constant_name: str, user_id: int) -> str | None:
+    """
+    Получение пользовательской константы по ее имени.
+
+    :param constant_name:   Имя константы в редис.
+    :param user_id:         ID пользователя.
+    :return:                Значение пользовательской константы.
+    """
+    name = constant_name.format(user_id=user_id)
+    date = await redis_client.get(name)
+    return date if date else None
+
+
+async def update_user_constant(constant_name: str, user_id: int, value: str) -> None:
+    """
+    Обновление пользовательской константы по ее имени.
+
+    :param constant_name:   Имя константы в редис.
+    :param user_id:         ID пользователя.
+    :param value:           Новое значение пользовательской константы.
+    """
+    name = constant_name.format(user_id=user_id)
+    await redis_client.set(name, value)
+
+
+async def del_user_constant(constant_name: str, user_id: int) -> None:
+    """
+    Удаление пользовательской константы по ее имени.
+
+    :param constant_name:   Имя константы в редис.
+    :param user_id:         ID пользователя.
+    """
+    name = constant_name.format(user_id=user_id)
+    await redis_client.delete(name)
